e95b7106277df13cd83cf84db05f0cd024cca710
Joanna <astronautnemo@gmail.com>
2023-03-14 11:34:06 +0300

    completed Video: Rails Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :shops
+  # custom routes
+  # http verb, url, controller#action
+  get "pets", to: "pets#index"
+
+  # resource method: generates 7 route that follow REST convention
+  resource :shops
+  resource :pets, only: [:show, :create, :new, :edit, :update]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
